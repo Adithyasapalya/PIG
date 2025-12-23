9f45131bf957880465591a2be7a1209aa325f624
Adithya Sapalya <adithyasapalya30@gmail.com>
2025-12-23 20:52:57 +0530

    player index added

--- pig.py
+++ pig.py
@@ -21,4 +21,28 @@ while True:
 max_score = 50
 player_scores = [0 for _ in range(players)]
 
-print(player_scores)
+
+while max(player_scores) < max_score:
+    for players_idx in range(players):
+        print("\nPlayer Number", players_idx + 1, "turn has just started.!!\n")
+        current_score = 0
+
+        while True:
+            should_roll = input("Would you like to roll? (y/n): ").lower()
+            if should_roll.lower() != 'y':
+                break
+            
+            value = roll()
+            if value == 1:
+                current_score = 0
+                print("You rolled a 1! No points this turn.")
+                break
+
+            else:
+                current_score += value
+                print(f"You rolled a {value}!")
+
+            print(f"Your current score this turn is: {current_score}")
+            
+        player_scores[players_idx] += current_score
+        print("Your total score is:", player_scores[players_idx])
